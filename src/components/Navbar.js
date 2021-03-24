b9b257ac0a22b7fed38019e96c42908add96dbe4
Shubham Pandey <60984701+ShubhamPnd@users.noreply.github.com>
2021-03-24 23:48:11 +0530

    Added Home Icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,7 @@ function Navbar() {
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                Home
+                <i class="fas fa-home"></i> Home
               </Link>
             </li>
             <li className='nav-item'>
